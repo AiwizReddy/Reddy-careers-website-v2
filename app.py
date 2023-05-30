3451f012a03550364f1e20e243a65277159657c4
AiwizReddy <111725569+AiwizReddy@users.noreply.github.com>
2023-05-30 15:13:04 +0000

    adedd html,css using bootstrap , dynamic data and created api route

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 app = Flask(__name__)
-
+JOBS=[{"id":1,"jobprofile":"Data analyst","location":"Bengaluru,india","salary":"10,00,000"},{"id":2,"jobprofile":"Data scientist","location":"Delhi,india","salary":"15,00,000"},{"id":3,"jobprofile":"Data Engineer","location":"Remote,india","salary":"12,00,000"},{"id":4,"jobprofile":"Frontend Engineer","location":"Hydrabad,india","salary":"9,00,000"}]
 
 @app.route("/")
 def hello_reddy():
-  return "Hello-reddy"
+  return render_template ('home.html',jobs=JOBS)
 
+@app.route("/api/json")
+def list_jobs():
+  jsonify(JOBS)
 if __name__== "__main__" :
   app.run(host="0.0.0.0",debug=True)
